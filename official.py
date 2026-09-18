de2942cfe7f6a2ff20f13389c1f1cde79989f85b
kawasaki8228brother-ship-it <kawasaki8228brother@gmail.com>
2026-09-19 06:01:26 +0900

    Wire lossless beforeinfo adapter into C fetcher without changing completeness policy

--- official.py
+++ official.py
@@ -9,7 +9,8 @@ from typing import Dict, Optional
 import httpx
 
 from models import JST, LiveDataCompleteness, OfficialRaceInfo, RaceIndexSnapshot, SourceEvidence
-from parsers import ParserError, parse_beforeinfo, parse_race_index
+from parsers import ParserError, parse_race_index
+from beforeinfo_bridge import prepare_beforeinfo
 from odds_bridge import parse_c_odds
 
 logger = logging.getLogger("gamagori-controller")
@@ -181,7 +182,7 @@ class OfficialDataFetcher:
 
         async def fetch_before():
             html, acquired_at = await self._get_text(before_url)
-            return parse_beforeinfo(html, before_url, acquired_at)
+            return prepare_beforeinfo(html, before_url, acquired_at).live_data
 
         async def fetch_odds():
             html, acquired_at = await self._get_text(odds_url)
@@ -203,7 +204,9 @@ class OfficialDataFetcher:
         else:
             result.exhibition_times = before_result.exhibition_times
             result.entry_courses = before_result.entry_courses
+            # Compatibility projection only: not a six-boat coverage field.
             result.start_exhibition_st = before_result.start_exhibition_st
+            result.start_exhibition_readings = dict(before_result.start_exhibition_readings)
             result.weather_info = before_result.weather_info
             result.source_evidence.update(before_result.source_evidence)
             missing.extend(before_result.missing_fields)
